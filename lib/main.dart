7a6cdf140d69b66ef99955070c5807b43afff337
Daniel Bahia <booude@gmail.com>
2023-08-25 23:54:33 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:learning_flutter_firebase/utils.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Video Upload'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? _videoURL;
+  VideoPlayerController? _controller;
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: _videoURL != null
+            ? _videoPreviewWidget()
+            : const Text('No Video Selected'),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _pickVideo,
+          child: const Icon(
+            Icons.video_library,
+          )),
+    );
+  }
+
+  void _pickVideo() async {
+    _videoURL = await pickVideo();
+    _initializeVideoPlayer();
+  }
+
+  void _initializeVideoPlayer() {
+    _controller = VideoPlayerController.file(File(_videoURL!))
+      ..initialize().then((_) {
+        setState(() {});
+        _controller!.play();
+      });
+  }
+
+  Widget _videoPreviewWidget() {
+    if (_controller != null) {
+      return AspectRatio(
+        aspectRatio: _controller!.value.aspectRatio,
+        child: VideoPlayer(_controller!),
+      );
+    } else {
+      return const CircularProgressIndicator();
+    }
+  }
+}
